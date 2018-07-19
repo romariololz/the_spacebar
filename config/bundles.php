c89e8e94540105313f7621db461fb0fd1155c1d0
Helou Romain <helouromain@gmail.com>
2018-07-19 21:53:15 +0200

    28 - MakerBundle
    composer require maker --dev
    
    To get commands from the maker use : "php bin/console"

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
     Nexy\SlackBundle\NexySlackBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
