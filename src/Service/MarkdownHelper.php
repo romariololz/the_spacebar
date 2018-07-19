9f224f029d1ee438cff06293b41032f188714123
Helou Romain <helouromain@gmail.com>
2018-07-19 17:07:44 +0200

    20 - Using Non-Standard Services: Logger Channels
    To pass the service with this id in yaml configurations file, use a special Symfony syntax "@".

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -10,22 +10,26 @@ namespace App\Service;
 
 
 use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class MarkdownHelper
 {
     private $cache;
     private $markdown;
+    private $logger;
 
     /**
      * MarkdownHelper constructor.
      * @param AdapterInterface $cache
      * @param MarkdownInterface $markdown
+     * @param LoggerInterface $logger
      */
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown)
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $logger)
     {
         $this->cache = $cache;
-        $this->markdown= $markdown;
+        $this->markdown = $markdown;
+        $this->logger = $logger;
     }
 
     /**
@@ -35,6 +39,11 @@ class MarkdownHelper
      */
     public function parse(string $source): string
     {
+        if (stripos($source, 'bacon') !== false)
+        {
+            $this->logger->info('They are talking about bacon again!');
+        }
+
         $item = $this->cache->getItem('markdown_'.md5($source));
         if (!$item->isHit())
         {
