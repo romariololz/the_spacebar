f4fba5056ea3598c23846f927fdc09a28074de94
Helou Romain <helouromain@gmail.com>
2018-07-20 22:26:01 +0200

    35 - Fun with Twig Extensions!
    php bin/console make:twig-extension.

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Twig;
+
+use App\Service\MarkdownHelper;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class AppExtension extends AbstractExtension
+{
+    private $helper;
+
+    public function __construct(MarkdownHelper $helper)
+    {
+        $this->helper = $helper;
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('cached_markdown', [$this, 'processMarkdown'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    /**
+     * @param $value
+     * @return string
+     * @throws \Psr\Cache\InvalidArgumentException
+     */
+    public function processMarkdown($value)
+    {
+        return $this->helper->parse($value);
+    }
+}
