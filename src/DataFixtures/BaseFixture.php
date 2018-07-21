382bcb469858be969785f50269bd54faf6ed9cc9
Helou Romain <helouromain@gmail.com>
2018-07-21 13:40:39 +0200

    43 - Fixtures: Seeding Dummy Data!
    composer require orm-fixtures --dev
    
    php bin/console make:fixtures
    
    php bin/console doctrine:fixtures:load

---
+++ src/DataFixtures/BaseFixture.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romariololz
+ * Date: 21/07/18
+ * Time: 12:55
+ */
+
+namespace App\DataFixtures;
+
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+abstract class BaseFixture extends Fixture
+{
+    /**
+     * @var ObjectManager
+     */
+    private $manager;
+
+    abstract protected function loadData(ObjectManager $em);
+
+    public function load(ObjectManager $manager)
+    {
+        $this->manager = $manager;
+
+        $this->loadData($manager);
+    }
+
+    public function createMany(string $className, int $count, callable $factory)
+    {
+        for ($i = 0; $i < $count; $i++)
+        {
+            $entity = new $className();
+            $factory($entity, $i);
+
+            $this->manager->persist($entity);
+            // store for usage later as App\Entity\ClassName_#COUNT#
+            $this->addReference($className . '_' . $i, $entity);
+        }
+    }
+}
