6261df19399c4ff64dbc2cd0866179e0f3c58581
Helou Romain <helouromain@gmail.com>
2018-07-21 12:29:39 +0200

    41 - Updating an Entity with New Fields
    php bin/console make:entity
    
    php bin/console make:migration
    
    php bin/console doctrine:migrations:migrate
    
    To delete the table entirely and start fresh: php bin/console doctrine:query:sql "TRUNCATE TABLE article"

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -21,7 +21,7 @@ class ArticleAdminController extends AbstractController
         $article = new Article();
         $article->setTitle('Why Asteroids Taste Like Bacon')
             ->setSlug('why-asteroids-taste-like-bacon'.rand(100,999))
-            ->setContent('<<< EOF
+            ->setContent('
 Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
 lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
 labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
@@ -37,13 +37,17 @@ Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamb
 mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
 strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
 cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
-fugiat.
-EOF');
+fugiat.');
+
 
         if (rand(1,10) > 2)
         {
             $article->setPublishedAt(new \DateTime(sprintf('-%d days', rand(1,100))));
         }
+
+        $article->setAuthor('Mike Ferengi')
+            ->setHeartCount(rand(5,100))
+            ->setImageFilename('asteroid.jpeg');
 
         $em->persist($article);
         $em->flush();
