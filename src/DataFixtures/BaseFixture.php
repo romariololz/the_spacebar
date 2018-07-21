26bfd2e0a06b2d570315088684e3a34310bfe952
Helou Romain <helouromain@gmail.com>
2018-07-21 13:53:29 +0200

    44 - Using Faker for Seeding Data
    composer require fzaninotto/faker --dev

--- src/DataFixtures/BaseFixture.php
+++ src/DataFixtures/BaseFixture.php
@@ -11,6 +11,8 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
 
 abstract class BaseFixture extends Fixture
 {
@@ -19,11 +21,17 @@ abstract class BaseFixture extends Fixture
      */
     private $manager;
 
+    /**
+     * @var Generator
+     */
+    protected $faker;
+
     abstract protected function loadData(ObjectManager $em);
 
     public function load(ObjectManager $manager)
     {
         $this->manager = $manager;
+        $this->faker = Factory::create();
 
         $this->loadData($manager);
     }
