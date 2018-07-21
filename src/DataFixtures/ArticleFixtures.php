7972564bcefbdfeb0db40836417664303d8c8f5d
Helou Romain <helouromain@gmail.com>
2018-07-21 15:37:58 +0200

    50 - Saving Relations

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\Comment;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class ArticleFixtures extends BaseFixture
@@ -24,7 +25,7 @@ class ArticleFixtures extends BaseFixture
 
     protected function loadData(ObjectManager $manager)
     {
-        $this->createMany(Article::class, 10, function($article, $count){
+        $this->createMany(Article::class, 10, function($article, $count) use ($manager){
             $article->setTitle($this->faker->randomElement(self::$articleTitles))
                 ->setContent('
 Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
@@ -52,6 +53,18 @@ fugiat.');
             $article->setAuthor($this->faker->randomElement(self::$articleAuthors))
                 ->setHeartCount($this->faker->numberBetween(5, 100))
                 ->setImageFilename($this->faker->randomElement(self::$articleImages));
+
+            $comment1 = new Comment();
+            $comment1->setAuthorName('Mike Ferengi');
+            $comment1->setContent('I ate a normal rock once. It did NOT taste like bacon!');
+            $comment1->setArticle($article);
+            $manager->persist($comment1);
+
+            $comment2 = new Comment();
+            $comment2->setAuthorName('Mike Ferengi');
+            $comment2->setContent('Woohoo! I\'m going on all-asteroid diet!');
+            $comment2->setArticle($article);
+            $manager->persist($comment2);
         });
 
         $manager->flush();
