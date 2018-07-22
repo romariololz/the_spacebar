ba58570ced07c41c0c391e323b52cebb76282e09
Helou Romain <helouromain@gmail.com>
2018-07-22 14:03:12 +0200

    57 - Collection Magic with Criteria
    Doctrine has a super powerful and amazing feature called Criteria.
    
    Instead of looping over all the data, add $criteria = Criteria - the one from Doctrine - Criteria::create().
    
    The Criteria object is similar to the QueryBuilder we're used to, but with a slightly different, well, slightly more confusing syntax.
    
    By using Criteria, we get super efficient filtering.
    
    We can also re-use Criteria in a QueryBuilder because of a static call.

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Article;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\QueryBuilder;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
@@ -30,6 +31,15 @@ class ArticleRepository extends ServiceEntityRepository
             ->getQuery()
             ->getResult()
         ;
+    }
+
+    public static function createNonDeletedCriteria(): Criteria
+    {
+        $criteria = Criteria::create()
+            ->andWhere(Criteria::expr()->eq('isDeleted', false))
+            ->orderBy(['createdAt' => 'DESC']);
+
+        return $criteria;
     }
 
 
